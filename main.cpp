6c8f3911f0cc38c64856b60c5a3b084dd26bd007
schluri <leo.schuwerk@gmail.com>
2017-03-20 11:31:04 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml>
+
+#include "field.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    Field field;
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("game", &field);
+
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
